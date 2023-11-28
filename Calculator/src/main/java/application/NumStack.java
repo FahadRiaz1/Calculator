a3bd98add40ddcb8e86c8f830c914c34ff799671
Fahad Riaz <wlis097@live.rhul.ac.uk>
2023-11-28 01:06:25 +0000

    TDD: Added stack field, NumStack constructor and size method

--- Calculator/src/main/java/application/NumStack.java
+++ Calculator/src/main/java/application/NumStack.java
@@ -1,5 +1,15 @@
 package application;
 
 public class NumStack {
+  
+  private Stack numStack;
+  
+  public NumStack() {
+    this.numStack = new Stack();
+  }
+  
+  public int size() {
+    return numStack.size();
+  }
 
 }
--- Calculator/src/test/java/application/NumStackTest.java
+++ Calculator/src/test/java/application/NumStackTest.java
@@ -1,16 +1,29 @@
 package application;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class NumStackTest {
   
   private NumStack numStack;
+  
+  @BeforeEach
+  public void setUp() {
+    numStack = new NumStack();
+  }
+
 
   @Test
   public void testNumStackClassObjectCreation() {
     numStack = new NumStack();
     assertNotNull(numStack);
+
+  }
+
+  @Test
+  public void numStackShouldHaveSizeZeroUponInitialization() {
+    assertEquals(0, numStack.size());
 
   }
 
