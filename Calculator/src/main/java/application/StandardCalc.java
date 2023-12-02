23bb8f49ade00b51af30ef3ad59172b558922ce7
Fahad Riaz <wlis097@live.rhul.ac.uk>
2023-12-02 01:27:54 +0000

    Added the class fields and constructor

--- Calculator/src/main/java/application/StandardCalc.java
+++ Calculator/src/main/java/application/StandardCalc.java
@@ -2,4 +2,14 @@ package application;
 
 public class StandardCalc {
 
+  private NumStack numStack;
+  private OpStack opStack;
+  private RevPolishCalc rpCalc;
+
+  public StandardCalc() {
+    numStack = new NumStack();
+    opStack = new OpStack();
+    rpCalc = new RevPolishCalc();
+  }
+
 }
