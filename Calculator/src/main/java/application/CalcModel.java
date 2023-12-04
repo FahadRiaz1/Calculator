ea2dfa1a3d2b4d141491f133dcc3a6a0979ede1a
Fahad Riaz <wlis097@live.rhul.ac.uk>
2023-12-04 03:29:48 +0000

    Added useful javadoc comments to CalculatorFactory and CalcModel classes

--- Calculator/src/main/java/application/CalcModel.java
+++ Calculator/src/main/java/application/CalcModel.java
@@ -2,19 +2,35 @@ package application;
 
 /**
  * Evaluates an expression - the evaluation can be Standard (infix) or reverse polish.
+ * 
+ * @author Fahad Riaz
  */
 public class CalcModel implements Calculator {
 
   private boolean isInfix;
 
+  /**
+   * Constructs a CalcModel instance with default calculation mode set to infix.
+   * 
+   */
   public CalcModel() {
     isInfix = true;
   }
 
+  /**
+   * Sets the calculation mode of the model.
+   * 
+   * @param isInfix If true, calculator is set to infix mode, otherwise RPN mode
+   */
   public void setInfix(boolean isInfix) {
     this.isInfix = isInfix;
   }
 
+  /**
+   * Evaluates the given expression based on the current calculation mode. It uses the factory to
+   * create an appropriate calculator based on the infix flag and gives evaluation to it.
+   *
+   */
   @Override
   public float evaluate(String expression)
       throws InvalidExpression, EmptyStackException, BadTypeException {
--- Calculator/src/main/java/application/CalculatorFactory.java
+++ Calculator/src/main/java/application/CalculatorFactory.java
@@ -1,7 +1,21 @@
 package application;
 
+/**
+ * Factory class for creating instances of different types of calculators. This class encapsulates
+ * the logic for instantiating calculator objects, allowing for easy creation of instances based on
+ * different calculation modes.
+ * 
+ * @author Fahad Riaz
+ * 
+ */
 public class CalculatorFactory {
 
+  /**
+   * Creates and returns a Calculator instance based on the provided calculation mode.
+   * 
+   * @param isInfix A boolean value indicating the type of calculator to be created.
+   * @return calculator instance of either StandardCalc or RevPolishCalc based on provided mode
+   */
   public static Calculator createCalculator(boolean isInfix) {
     if (isInfix) {
       return new StandardCalc();
