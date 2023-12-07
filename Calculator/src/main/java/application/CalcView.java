7fa85a497d087c2c5fc02d6228aaf22345738466
Fahad Riaz <wlis097@live.rhul.ac.uk>
2023-12-07 06:10:46 +0000

    Further improved the overall GUI of my calculator in prep for the final release

--- Calculator/src/main/java/application/CalcView.java
+++ Calculator/src/main/java/application/CalcView.java
@@ -71,7 +71,7 @@ public class CalcView extends Application implements ViewInterface {
     Scene scene = new Scene(page);
     scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
     primaryStage.setScene(scene);
-    primaryStage.setTitle("Calculator by Fahad Riaz");
+    primaryStage.setTitle("Calculator v3.0");
     primaryStage.show();
   }
 
